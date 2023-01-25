cde06e264c6f61dc71c42daf6fc2a62a37dc7632
Batcleta <35137108+Batcleta@users.noreply.github.com>
2023-01-25 12:24:21 -0300

    header done

--- ui-ux-responsive-website/src/containers/header/index.tsx
+++ ui-ux-responsive-website/src/containers/header/index.tsx
@@ -1,6 +1,31 @@
 import './main.css'
 import { FC } from 'react'
+import people from '../../assets/people.png'
+import ai from '../../assets/ai.png'
 
 export const Header: FC = () => {
-  return <div>Header</div>
+  return (
+    <div className='gpt3__header section__padding' id='home'>
+      <div className='gpt3__header-content'>
+        <h1 className='gradient__text'>Lets Build Somethig Amazing with GPT-3 OpenAI</h1>
+        <p>
+          Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise
+          blessing. Indulgence way everything joy alteration boisterous the attachment. Party we
+          years to order allow asked of.
+        </p>
+        <div className='gpt3__header-content_input'>
+          <input type='email' name='email' id='' placeholder='Your Email Address' />
+          <button type='button'>Get Started</button>
+        </div>
+
+        <div className='gpt3__header-content_people'>
+          <img src={people} alt='people' />
+          <p>1,600 people requested access a visit in last 24 hours</p>
+        </div>
+      </div>
+      <div className='gpt3__header-image'>
+        <img src={ai} alt='' />
+      </div>
+    </div>
+  )
 }
