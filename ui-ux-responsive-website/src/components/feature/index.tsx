23edd510dcee17ec6807272a7a4a74e603209d5a
Batcleta <35137108+Batcleta@users.noreply.github.com>
2023-01-25 15:14:58 -0300

    future features done

--- ui-ux-responsive-website/src/components/feature/index.tsx
+++ ui-ux-responsive-website/src/components/feature/index.tsx
@@ -7,12 +7,12 @@ interface IFeatureProps {
 }
 export const Feature: FC<IFeatureProps> = ({ title, text }) => {
   return (
-    <div className='gpt3__features-container__feature'>
-      <div className='gpt3__features-container__feature-title'>
+    <div className='gpt3__feature-container__feature'>
+      <div className='gpt3__feature-container__feature-title'>
         <div />
         <h1>{title}</h1>
       </div>
-      <div className='gpt3__features-container__feature-text'><p>{text}</p></div>
+      <div className='gpt3__feature-container__feature-text'><p>{text}</p></div>
     </div>
   )
 }
