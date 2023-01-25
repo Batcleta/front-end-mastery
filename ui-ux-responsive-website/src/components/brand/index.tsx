ef20c15536bc6a1eba667bb754f14c65f3099f13
Batcleta <35137108+Batcleta@users.noreply.github.com>
2023-01-25 12:40:19 -0300

    brand section ok

--- ui-ux-responsive-website/src/components/brand/index.tsx
+++ ui-ux-responsive-website/src/components/brand/index.tsx
@@ -1,6 +1,13 @@
 import './main.css'
 import { FC } from 'react'
+import { atlassian, dropbox, google, shopify, slack } from './imports'
 
 export const Brand: FC = () => {
-  return <div>Brand</div>
+  return <div className='gpt3__brand section__padding'>
+    <div><img src={google} alt="google" /></div>
+    <div><img src={slack} alt="slack" /></div>
+    <div><img src={atlassian} alt="atlassian" /></div>
+    <div><img src={dropbox} alt="dropbox" /></div>
+    <div><img src={shopify} alt="shopify" /></div>
+  </div>
 }
