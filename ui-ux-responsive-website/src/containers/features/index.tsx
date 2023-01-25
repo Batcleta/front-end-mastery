c64987d2203998527e03ac3874cc34deb89cd17c
Batcleta <35137108+Batcleta@users.noreply.github.com>
2023-01-25 15:02:08 -0300

    refactor: making features more readable

--- ui-ux-responsive-website/src/containers/features/index.tsx
+++ ui-ux-responsive-website/src/containers/features/index.tsx
@@ -2,6 +2,30 @@ import './main.css'
 import { FC } from 'react'
 import { Feature } from '../../components'
 
+interface IFeaturesData {
+  title: string
+  text: string
+}
+
+const feturesData: IFeaturesData[] = [
+  {
+    title: 'Improving end distrusts instantly',
+    text: 'From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly was household applauded.',
+  },
+  {
+    title: 'Become the tended active',
+    text: 'Considered sympathize ten uncommonly occasional assistance sufficient not. Letter of on become he tended active enable to.',
+  },
+  {
+    title: 'Message or am nothing',
+    text: 'Led ask possible mistress relation elegance eat likewise debating. By message or am nothing amongst chiefly address.',
+  },
+  {
+    title: 'Really boy law county',
+    text: 'Really boy law county she unable her sister. Feet you off its like like six. Among sex are leave law built now. In built table in an rapid blush.',
+  },
+]
+
 export const Features: FC = () => {
   return (
     <div className='gpt3__features section__padding'>
@@ -14,22 +38,10 @@ export const Features: FC = () => {
         <p>Request Early Access to Get Started</p>
       </div>
       <div className='gpt3__features-container'>
-        <Feature
-          title='Improving end distrusts instantly '
-          text='From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly was household applauded.'
-        />
-        <Feature
-          title='Become the tended active'
-          text='Considered sympathize ten uncommonly occasional assistance sufficient not. Letter of on become he tended active enable to.'
-        />
-        <Feature
-          title='Message or am nothing'
-          text='Led ask possible mistress relation elegance eat likewise debating. By message or am nothing amongst chiefly address.'
-        />
-        <Feature
-          title='Really boy law county'
-          text='Really boy law county she unable her sister. Feet you off its like like six. Among sex are leave law built now. In built table in an rapid blush.'
-        />
+        {feturesData &&
+          feturesData.map(({ title, text }, index) => (
+            <Feature key={index} title={title} text={text} />
+          ))}
       </div>
     </div>
   )
