f5121ed8da16ca835ab5c98a3bf9a461ca82ab7a
Washington Ferreira <35137108+Batcleta@users.noreply.github.com>
2023-01-26 13:50:01 -0300

    footering

--- ui-ux-responsive-website/src/containers/footer/index.tsx
+++ ui-ux-responsive-website/src/containers/footer/index.tsx
@@ -1,6 +1,44 @@
 import './main.css'
 import { FC } from 'react'
+import gpt3Logo from '../../assets/logo.svg'
 
 export const Footer: FC = () => {
-  return <div>Footer</div>
+  return (
+    <div className='gpt3__footer sectin__padding'>
+      <div className='gpt3__footer-heading'>
+        <h1 className='gradient__text'>Do you want to step in to the future before others</h1>
+      </div>
+      <div className='gpt3__footer-btn'>
+        <p>Request Early Access</p>
+      </div>
+      <div className='gpt3__footer-links'>
+        <div className='gpt3__footer-links__logo'>
+          <img src={gpt3Logo} alt='logo' />
+          <p>all rights reserved</p>
+        </div>
+        <div className='gpt3__footer-links__div'>
+          <h4>Links</h4>
+          <p>Overons</p>
+          <p>Social Media</p>
+          <p>Counters</p>
+          <p>Contact</p>
+        </div>
+        <div className='gpt3__footer-links__div'>
+          <h4>Company</h4>
+          <p>Terms & conditions</p>
+          <p>Privacy policy</p>
+          <p>Contact</p>
+        </div>
+        <div className='gpt3__footer-links__div'>
+          <h4>Get in touch</h4>
+          <p>asdasdasdasd asdasdas dasd</p>
+          <p>+5511963642358</p>
+          <p>thewazig.wf@gmail.com</p>
+        </div>
+      </div>
+      <div className='gpt3__footer-copyright'>
+        <p>All rights reserved</p>
+      </div>
+    </div>
+  )
 }
