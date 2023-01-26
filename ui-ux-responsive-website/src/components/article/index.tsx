c8e1f136fddd51c4d5745aef1771467550435612
Washington Ferreira <35137108+Batcleta@users.noreply.github.com>
2023-01-26 13:33:59 -0300

    blog articles are done

--- ui-ux-responsive-website/src/components/article/index.tsx
+++ ui-ux-responsive-website/src/components/article/index.tsx
@@ -1,6 +1,25 @@
 import './main.css'
 import { FC } from 'react'
 
-export const Article: FC = () => {
-  return <div>Article</div>
+interface IArticleProps {
+  imageUrl: string
+  date: string
+  title: string
+}
+
+export const Article: FC<IArticleProps> = ({ imageUrl, date, title }) => {
+  return (
+    <article className='gpt3__blog-container__article'>
+      <div className='gpt3__blog-container__article-image'>
+        <img src={imageUrl} alt='blog' />
+      </div>
+      <div className='gpt3__blog-container__article-content'>
+        <div>
+          <p>{date}</p>
+          <h3>{title}</h3>
+        </div>
+        <p>Read the full article</p>
+      </div>
+    </article>
+  )
 }
